d809e9bc7c856bf1c730b4797a82c1f4770975f2
HuynhPT <danghuynh3009@gmail.com>
2022-09-05 18:05:57 +0700

    up

--- src/screens/home/ScreenHome.tsx
+++ src/screens/home/ScreenHome.tsx
@@ -1,10 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {IconButton, MD3Colors} from 'react-native-paper';
 
 const ScreenHome = () => {
   return (
-    <View style={{ flex: 1}}>
+    <View style={{flex: 1 ,alignItems:'center', justifyContent:'center'}}>
       <Text>ScreenHome</Text>
+      <IconButton
+        icon="camera"
+        iconColor={MD3Colors.error50}
+        size={20}
+        onPress={() => console.log('Pressed')}
+      />
     </View>
   );
 };
